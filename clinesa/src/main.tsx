e31a54743cd2f41a33d00b7cec75112cf41afe22
Luis <luis.ros.moreno2@gmail.com>
2025-09-06 11:04:55 +0200

    fix: Remove duplicate Router causing navigation error
    - Remove BrowserRouter from main.tsx to prevent Router inside Router error
    - Keep Router only in App.tsx where it belongs
    - Fixes 'You cannot render a Router inside another Router' error
    - Application now has single Router context as required by React Router

--- clinesa/src/main.tsx
+++ clinesa/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 // import { Analytics } from '@vercel/analytics/react'
 import './index.css'
@@ -13,10 +12,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-      {/* <Analytics /> */}
-    </BrowserRouter>
+    <App />
+    <Toaster />
+    {/* <Analytics /> */}
   </StrictMode>,
 )
